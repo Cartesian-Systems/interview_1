2e1705be8ae86d9cb7610bb4bdb57851d1ed583f
narashbringer <jspyreas@yahoo.com>
2023-08-10 09:34:45 -0400

    make shorter

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 
 
-easy = """
-[2023-07-26 12:34:56] [INFO] User logged in successfully.
+easy = """[2023-07-26 12:34:56] [INFO] User logged in successfully.
 [2023-07-26 12:35:20] [ERROR] Connection refused by the server.
 [2023-07-26 12:36:15] [INFO] User data updated.
-[2023-07-26 12:37:05] [INFO] User logged in successfully.
-"""
+[2023-07-26 12:37:05] [INFO] User logged in successfully."""
 
-hard = """
-[2023-07-26 10:15:23] [INFO] Application started
+hard = """[2023-07-26 10:15:23] [INFO] Application started
 [2023-07-26 10:15:30] [ERROR] Invalid input received
 [2023-07-26 10:15:35] [DEBUG] Processing data
 [2023-07-26 10:15:42] [WARNING] Connection timed out
@@ -27,21 +24,39 @@ hard = """
 [2023-07-26 10:17:25] [INFO] Payment received from John Doe
 [2023-07-26 10:17:32] [ERROR] Unable to process request
 [2023-07-26 10:17:40] [INFO] Configuration saved
-[2023-07-26 10:17:48] [DEBUG] Checking permissions
-"""
+[2023-07-26 10:17:48] [DEBUG] Checking permissions"""
 
-fail = """
-[2023-07-26 12:34:56] [INFO] User logged in successfully.
+fail = """[2023-07-26 12:34:56] [INFO] User logged in successfully.
 [2023-07-26 12:35:20]] [ERROR] Connection refused by the server.
 [2023-07-26 12:36:15] [INFO] User data updated.
-[2023-07-26 12:37:05] [INFO] User logged in successfully.
-"""
+[2023-07-26 12:37:05] [INFO] User logged in successfully."""
 
 
 def parse_lines(lines: str):
     """Parse lines into a the desired parts"""
-    pass
+    count = 0
+    log_level_counts = None
+    earliest_timestamp = None
+    latest_timestamp = None
+    most_frequent_word = None
+    unique_words = None
 
+    for line in lines.splitlines():
+       pass
+
+    print_results(count, log_level_counts, earliest_timestamp, latest_timestamp, most_frequent_word, unique_words)
+
+
+def print_results(count: int, log_level_counts: dict, 
+                  earliest_timestamp: str, latest_timestamp: str,
+                  most_frequent_word: str, unique_words: set):
+    """Print the results"""
+    print(f"Total log entries: {count}")
+    print(f"Log level counts: {log_level_counts}")
+    print(f"Earliest timestamp: {earliest_timestamp}")
+    print(f"Latest timestamp: {latest_timestamp}")
+    print(f"Most frequent word: {most_frequent_word}")
+    print(f"Unique words: {unique_words}")
 
 parse_lines(easy)
 print("------------------")
